24a1a8bf6db6a1f87d220615020cf7c16fc6170f
Leo <oleoneto@gmail.com>
2018-04-17 03:01:12 -0400

    Added project name, `Django-Autogenerator`, to the file.

--- generator.sh
+++ generator.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Requirements: Assumes Python3.6, Django 2.0.0, and Git
+# Django-Autogenerator
 # Requirements: Assumes Python3.6, PIP, Django 2.0.0, and Git
 # A handy script for creating Django Projects and their respective applications
 # Give this file `755` permissions in order to be able to run it.
