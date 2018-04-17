a2d6dbf7e3874f959c5efc59ca79ce77ab9bf4f0
Leo <oleoneto@gmail.com>
2018-04-17 02:55:28 -0400

    Adds sample Audio model.

--- Templated/_appfiles_/models.py
+++ Templated/_appfiles_/models.py
@@ -15,3 +15,21 @@ class ModelName(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Audio(models.Model):
+
+    # An audio object related to the portfolio project.
+    source = models.FileField(upload_to='uploads/audios/', max_length=60)
+    number = models.IntegerField(blank=True)
+    title = models.CharField(max_length=50, blank=False)
+    artist = models.CharField(max_length=50, blank=False)
+    composer = models.CharField(max_length=50, blank=True)
+    genre = models.CharField(max_length=15, blank=True)
+    slug = models.SlugField(max_length=50, blank=True)
+
+    class Meta:
+        ordering = ['artist']
+
+    def __str__(self):
+        return self.title
