78fd93544378c590284a1896d88bd062deff2a51
Leo <oleoneto@gmail.com>
2018-11-08 17:23:57 -0500

    Change default templates

--- commands/helpers/templater.py
+++ commands/helpers/templater.py
@@ -8,57 +8,103 @@ p = inflect.engine()
 attribute_template = JTemplate("""{{ name }} = models.{{ type }}({{ options }})""")
 
 
+model_simple_template = JTemplate("""
+class {{ model | capitalize }}(models.Model):
+    {% for attribute in attributes %}{{ attribute }}
+    {% endfor %}
+""")
+
 model_template = JTemplate("""
-class {{ model }}(models.Model):
-
-	{% for attribute in attributes %}{% if loop.index > 1 %}\t{% endif %}{{ attribute }}\n{% endfor %}
-	class Meta:
-		db_table = '{{ model }}_table'
-
-	def __str__(self):
-		return None
+class {{ model | capitalize }}(models.Model):
+    {% for attribute in attributes %}{{ attribute }}
+    {% endfor %}
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    
+    class Meta:
+        db_table = '{{ model | capitalize }}_table'
+        ordering = ['-created_at']
+        {% if abstract %}abstract = True{% endif %}
+    
+    def __str__(self):
+        return {% if descriptor %}self.{{ descriptor }}{% else %}None{% endif %}
 """)
 
 
 model_admin_template = JTemplate("""
-class {{ model }}Admin(admin.ModelAdmin):
-	pass
-	
-admin.site.register({{ model }}, {{model}}Admin)
+class {{ model | capitalize }}Admin(admin.ModelAdmin):
+    pass
+admin.site.register({{ model | capitalize }}, {{model}}Admin)
 """)
 
 
 route_template = JTemplate("""
-def {{ route }}(request):
-	context = {
-		active: True,
-		route: '{{ route }}',
-	 }
-	return render(request, '{{ route }}.html', context)
+def {{ route | lower }}(request):
+    context = {
+        active: True,
+        route: '{{ route | lower }}',
+     }
+    return render(request, '{{ route | lower }}.html', context)
 """)
 
 
 form_template = JTemplate("""
-class {{ model }}Form(forms.Form):
-	class Meta:
-		model = {{ model }}
-		fields = "__all__"
+class {{ model | capitalize }}Form(forms.Form):
+    class Meta:
+        model = {{ model | capitalize }}
+        fields = "__all__"
 """)
 
 
 serializer_template = JTemplate("""
-class {{ model }}Serializer(serializers.ModelSerializer):
-	
-	# If model has foreign key to another model, add it like so:
-	# models = serializers.StringRelatedField(many=True)
-	
-	class Meta:
-		model = {{ model }}
-		fields = "__all__"
+class {{ model | capitalize }}Serializer(serializers.ModelSerializer):
+    # If model has foreign key to another model, add it like so:
+    # models = serializers.StringRelatedField(many=True)
+    
+    class Meta:
+        model = {{ model | capitalize }}
+        fields = "__all__"
 """)
 
 viewset_template = JTemplate("""
-class {{ model }}Viewset(viewsets.ModelViewSet):
-	queryset = {{ model }}.objects.all()
-	serializer_class = {{ model }}Serializer
+class {{ model | capitalize }}Viewset(viewsets.{% if read_only %}ReadOnlyModelViewSet{% else %}ModelViewSet{% endif %}):
+    queryset = {{ model }}.objects.all()
+    serializer_class = {{ model | capitalize }}Serializer
+""")
+
+html__simple_template = JTemplate("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{ route | capitalize }}</title>
+    {% if framework %}<link rel="stylesheet" href="{{ framework }}/css/{{ framework }}.css">
+    <script src="{{ framework }}/js/{{ framework }}.js"></script>{% endif %}
+    <link rel="stylesheet" href="static/css/main.css">
+</head>
+<body>
+    <h1>Template for {{ route | capitalize }}</h1>
+    
+    <script src="static/js/main.js"></script>
+</body>
+</html>
+""")
+
+html_template = JTemplate("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ route | capitalize }}</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    {% if framework %}<link rel="stylesheet" href="{{ framework[0]['link'] }}" integrity="{{ framework[0]['integrity']}}" crossorigin="anonymous">
+    <script src="{{ framework[1]['src'] }}" integrity="{{ framework[0]['integrity']}}" crossorigin="anonymous"></script>{% endif %}
+    <link rel="stylesheet" href="static/css/main.css">
+</head>
+<body>
+    <h1>Template for {{ route | capitalize }}</h1>
+    
+    <script src="static/js/main.js"></script>
+</body>
+</html>
 """)
