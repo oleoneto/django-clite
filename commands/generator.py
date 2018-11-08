ec9a43122f2d766842767c8f7162ff94b3554fee
Leo <oleoneto@gmail.com>
2018-11-08 18:34:04 -0500

    Implement generator

--- commands/generator.py
+++ commands/generator.py
@@ -1,7 +1,6 @@
 import click
 import shutil
 from commands.helpers.generator import *
-from jinja2 import Template
 
 
 @click.group()
@@ -14,10 +13,12 @@ def generate(ctx):
 
 
 @generate.command()
-@click.option('--no-admin', is_flag=True, help="Do not add model to admin site")
+@click.option('--admin', is_flag=True, help="Register model to admin site")
+@click.option('--abstract', is_flag=True, help="Creates an abstract model type")
+@click.option('--no-defaults', is_flag=True, help="Omit class Meta, created_at, updated_at, and __str__")
 @click.argument("name")
 @click.argument("attributes", nargs=-1, required=False)
-def model(no_admin, name, attributes):
+def model(admin, abstract, no_defaults, name, attributes):
     """
     Generates a model in models.py
     \f
@@ -25,10 +26,13 @@ def model(no_admin, name, attributes):
     \b
     User Char:name Date:birthday Float:height Email:email Image:photo
 
-    Also adds model to admin.site by default. One can optionally opt out by specifying the --no-admin flag.
+    `created_at` and `updated_at` are added to a model by default unless --no-defaults is specified.
+
+    If the model is to be added to admin.site one can optionally opt in by specifying the --admin flag.
     """
 
-    attribute_template = Template("{{name}} = models.{{type}}({{options}})")
+    _attributes_ = []
+    _descriptor_ = None
 
     if attributes:
         for attr in attributes:
@@ -36,16 +40,28 @@ def model(no_admin, name, attributes):
             attribute = type_for_token(attr)
 
             if attribute:
+                if attribute[1] == "CharField":
+                    _descriptor_ = attribute[0]
                 options = default_options_for_attribute(attribute, name)
                 at = attribute_template.render(name=attribute[0], type=attribute[1], options=options)
-                click.secho(at, fg="green")
+                _attributes_.append(at)
+
+    if no_defaults:
+        _model_ = model_simple_template.render(model=name, attributes=_attributes_)
+    else:
+        _model_ = model_template.render(model=name, abstract=abstract, attributes=_attributes_, descriptor=_descriptor_)
+
+    # TODO: Add file output
+    click.secho("Output to models.py", fg='green')
+    click.secho(_model_, fg='green')
 
 
 @generate.command()
 @click.option('--no-template', is_flag=True, help="Do not generate a template file")
 @click.option('--no-url', is_flag=True, help="Do not include in app's url patterns")
-@click.argument("name")
-def route(no_template, no_path, name):
+@click.option('--framework', help="Name of css framework to link in stylesheets", type=click.Choice(supported_frameworks))
+@click.argument("name", required=True)
+def route(no_template, no_url, framework, name):
     """
     Generates a route (or view)
     \f
@@ -54,32 +70,74 @@ def route(no_template, no_path, name):
     Unless --no-url is set, the command also adds the route to urls.py
 
     """
-    pass
+    name = name.lower()
+
+    if framework:
+        _f = links_for_framework(framework)
+        if _f:
+            framework = _f
+            _html_ = html_template.render(route=name, framework=framework)
+        else:
+            _html_ = html__simple_template.render(route=name, framework=framework)
+    else:
+        _html_ = html__simple_template.render(route=name)
+
+    _route_ = route_template.render(route=name)
+
+    # TODO: implement flag control
+
+    # TODO: test create_file_(filename, content)
+    append_to_file("view.py", _route_)
+
+    # TODO: test create_file_(filename, content)
+    create_file("{}.html".format(name), _html_)
 
 
 @generate.command()
-@click.argument("name")
-def viewset(name):
+@click.option('--read-only', is_flag=True, help="Support only support GET requests")
+@click.argument("name", required=True)
+def viewset(read_only, name):
     """
     Generates a viewset for a serializable model
     \f
     Checks for the existence of the serializable model in serializers.py
     before attempting to create a viewset for it. Aborts if serializer is not found.
     """
-    # shutil.copytree('blueprints', 'leo')
-    pass
+
+    name = name.capitalize()
+
+    _viewset_ = viewset_template.render(model=name, read_only=read_only)
+
+    # TODO: Add file output
+    click.secho("Output to viewsets.py", fg='green')
+    click.secho(_viewset_, fg='green')
 
 
 @generate.command()
-@click.option('--add-model', is_flag=True, help="Adds the model to models.py if it does not exist")
-@click.argument("name")
-def serializer(add_model, name):
+@click.argument("name", required=True)
+def serializer(name):
     """
     Generates a serializer for a given model
     \f
     Checks for the existence of the specified model in models.py
     before attempting to create a serializer for it. Aborts if model is not found.
     """
-    pass
+
+    # TODO: Add file output
+    _serializer_ = serializer_template.render(model=name)
+    click.secho("Output to serializers.py", fg='green')
+    click.secho(_serializer_, fg="green")
 
 
+@generate.command()
+@click.argument("name", required=True)
+def form(name):
+    """
+    Generates a model form
+    """
+
+    _form_ = form_template.render(model=name)
+
+    # TODO: Add file output
+    click.secho("Output to forms.py", fg='green')
+    click.secho(_form_, fg="green")
--- commands/helpers/generator.py
+++ commands/helpers/generator.py
@@ -1,68 +1,94 @@
+import shutil
+import os
 import json
-import jinja2
-import inflect
-from enum import Enum, unique
+from pprint import pprint
+from commands.helpers.templater import *
 
-p = inflect.engine()
 
 file = open('config/config.json')
 data = json.load(file)
 file.close()
+
+supported_fields = []
+supported_frameworks = []
 types = data['reserved_words']
-relationships = types[0]['relationships']
+relationships = data['reserved_words'][0]['relationships']
+frameworks = data['supported_frameworks']
 
-
-@unique
-class ModelType(Enum):
-    FK = "ForeignKey"
-    One = "OneToOneField"
+for i in frameworks[0].keys():
+    supported_frameworks.append(i)
 
 
 def type_for_token(token):
 
     token = token.split(":")
+    token[0] = token[0].capitalize()
+    token[1] = token[1].lower()
 
     for i in token:
         if not len(i) > 1:
             return None
 
-    for type in relationships:
-        if token[0] in type:
-            return token[1], type.get(token[0])
+    for _type_ in relationships:
+        if token[0] in _type_:
+            return token[1], _type_.get(token[0])
 
-    for type in types:
-        if token[0] in type:
-            return token[1], type.get(token[0])
+    for _type_ in types:
+        if token[0] in _type_:
+            return token[1], _type_.get(token[0])
     return None
-
-
-def tokenized(arg):
-    return arg.split(":")
-
-
-def contains_key(key, _list):
-    def contains(_list):
-        return key in _list.values()
-    return contains(_list)
 
 
 def default_options_for_attribute(attr, model):
 
-    name = attr[0]
-    type = attr[1]
+    _name_ = attr[0]
+    _type_ = attr[1]
 
     options = 'blank=True'
 
-    if type == "CharField":
+    if _type_ == "CharField":
         options += ", max_length=30"
-    elif type == "ImageField" or type == "FileField":
+    elif _type_ == "BooleanField":
+        options = "default=False"
+    elif _type_ == "ImageField" or _type_ == "FileField":
         options += ", upload_to=/uploads/"
-    elif type == "ForeignKey":
+    elif _type_ == "ForeignKey":
         related_name = p.plural(model.lower())
-        options = "{}, {}, {}".format(name.capitalize(), "related_name='{}'".format(related_name), "on_delete=models.DO_NOTHING")
-    elif type == "OneToOneField":
+        options = "{}, {}, {}".format(_name_.capitalize(), "related_name='{}'".format(related_name), "on_delete=models.DO_NOTHING")
+    elif _type_ == "OneToOneField":
         related_name = model.lower()
-        options = "{}, {}, {}".format(name.capitalize(), "related_name='{}'".format(related_name), "on_delete=models.CASCADE")
-    elif type == "ManyToManyField":
-        options = "{}".format(name.capitalize())
+        options = "{}, {}, {}".format(_name_.capitalize(), "related_name='{}'".format(related_name), "on_delete=models.CASCADE")
+    elif _type_ == "ManyToManyField":
+        options = "{}".format(_name_.capitalize())
     return options
+
+
+def links_for_framework(framework):
+    framework = framework.lower()
+
+    css = frameworks[0][framework]['css']
+    js = frameworks[0][framework]['js']
+
+    return css, js
+
+
+def create_file(filename, content):
+    _dir_ = os.getcwd()
+    os.chdir("tests/outputs")
+
+    with open(filename, 'w') as f:
+        f.write(content)
+    f.close()
+    os.chdir(_dir_)
+
+
+def append_to_file(filename, content):
+    _dir_ = os.getcwd()
+    os.chdir("tests/outputs")
+
+    with open(filename, 'a') as f:
+        f.write("\n")
+        f.write(content)
+        f.write("\n")
+    f.close()
+    os.chdir(_dir_)
