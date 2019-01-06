8e5a7484788e0cb0a0d3399cf9e549e8706f59f9
Leo <oleoneto@gmail.com>
2019-01-06 02:42:29 -0500

    Update requirements

--- app.py
+++ app.py
@@ -9,13 +9,8 @@ A CLI that handles creating and managing Django projects
 """
 
 import click
-import os
-from djangocli.cli.commands.configurator.main import configure
 from djangocli.cli.commands.destroyer.main import destroy
 from djangocli.cli.commands.generator.main import generate
-from djangocli.cli.commands.initializer.main import new
-from djangocli.cli.commands.installer.main import install
-from djangocli.cli.commands.migrator.main import db
 
 
 @click.group()
@@ -31,12 +26,8 @@ Commands should be added as sub-commands of the main click group.
 This ensures sub-commands can be chained and 
 run as `django-cli SUB-COMMAND COMMAND`
 """
-# main.add_command(configure)
-# main.add_command(db)
 main.add_command(destroy)
 main.add_command(generate)
-# main.add_command(new)
-# main.add_command(install)
 
 if __name__ == '__main__':
     main()
