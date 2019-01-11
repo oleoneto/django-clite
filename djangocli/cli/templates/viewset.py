760063377d336ead641fe15e1cd195f5dc8352a2
Leo <oleoneto@gmail.com>
2019-01-11 18:49:07 -0500

    Fix viewset template

--- djangocli/cli/templates/viewset.py
+++ djangocli/cli/templates/viewset.py
@@ -8,6 +8,6 @@ from ..serializers.{{ model.lower() }} import {{ model.capitalize() }}Serializer
 
 
 class {{ model.capitalize() }}ViewSet(viewsets.{% if read_only %}ReadOnlyModelViewSet{% else %}ModelViewSet{% endif %}):
-    queryset = {{ model }}.objects.all()
+    queryset = {{ model.capitalize() }}.objects.all()
     serializer_class = {{ model.capitalize() }}Serializer
 """)
