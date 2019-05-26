cb7b1561bb8fb959f9e7d8d7e2af7a532b59f18a
Leo Neto <lneto@sharpencx.com>
2019-05-26 18:30:38 -0400

    Update version, license, requirements, urls, scripts...

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import os
 
 setup(
     name="Django CLI",
-    version="0.0.alpha2",
+    version="0.0a3",
     author="Leo Neto",
     author_email="leo@ekletik.com",
-    url="https://github.com/oleoneto/Django-CLI",
+    url="https://github.com/oleoneto/django-cli",
     description="CLI for managing Django projects",
     keywords="django automate cli command line tools rails ember python framework devops",
     classifiers=[
@@ -22,21 +22,23 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    license="BSD",
     install_requires=[
-        'Click',
-        'django>=2.1.5'
+        'Click==7.0',
+        'inflect==2.1.0',
+        'django>=2.2.1'
     ],
     zip_safe=False,
     include_package_data=True,
     packages=find_packages(),
     project_urls={
-        "Documentation": "https://github.com/oleoneto/Django-CLI/",
-        "Source Code": "https://github.com/oleoneto/Django-CLI/",
+        "Documentation": "https://github.com/oleoneto/django-cli/",
+        "Source Code": "https://github.com/oleoneto/django-cli/",
     },
     entry_points={
         'console_scripts': [
-            'djangocli=app:main',
+            'D=app:main',
+            'django-cli=app:main'
         ]
-    },
-    scripts=['app.py']
+    }
 )
