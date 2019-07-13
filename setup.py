a3a54f1884d3fc0f9533687f765be93d5b6b4ed8
Leo Neto <lneto@sharpencx.com>
2019-07-13 01:53:25 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="django-clite",
-    version="0.0.9-beta.2",
+    version="0.0.9-beta.3",
     author="Leo Neto",
     author_email="leo@ekletik.com",
     url="https://bitbucket.org/oleoneto/django-clite",
