8df8fd97e49c5f3720d262c7c5c3fd78bf05da68
Leo Neto <oleoneto@gmail.com>
2019-07-22 16:55:44 +0000

    Merged in development (pull request #5)
    Development
    
    * Bump version
    
    * Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="django-clite",
-    version="0.0.9-beta.5",
+    version="0.0.9-beta.6",
     author="Leo Neto",
     author_email="leo@ekletik.com",
     url="https://bitbucket.org/oleoneto/django-clite",
