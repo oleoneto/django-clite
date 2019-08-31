d076d8b42d6f06684a0715801ef046f6a38b8fa1
Leo <oleoneto@gmail.com>
2019-08-30 20:02:29 -0400

    Version 0.1.0-beta.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="django-clite",
-    version="0.0.9-beta.9",
+    version="0.1.0-beta.0",
     author="Leo Neto",
     author_email="leo@ekletik.com",
     url="https://bitbucket.org/oleoneto/django-clite",
