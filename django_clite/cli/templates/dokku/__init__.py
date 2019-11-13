a06637bddfccb38edb37145fd21a9a130424fdd0
Leo Neto <oleoneto@gmail.com>
2019-11-13 02:03:35 -0500

    Remove release and postdeploy instructions for dokku

--- django_clite/cli/templates/dokku/__init__.py
+++ django_clite/cli/templates/dokku/__init__.py
@@ -4,8 +4,7 @@ from jinja2 import Template
 app_template = Template("""{
   "scripts": {
     "dokku": {
-      "predeploy": "/code/manage.py migrate --noinput",
-      "postdeploy": "/code/./notify.sh"
+      "predeploy": "/code/manage.py migrate --noinput"
     }
   }
 }
@@ -20,6 +19,4 @@ DOKKU_CHECKS_TIMEOUT={{ timeout }}
 dokku_scale_template = Template("""web={{ web }}""")
 
 
-procfile_template = Template("""release: ./release.sh
-web: gunicorn {{ project }}.wsgi
-""")
+procfile_template = Template("""web: gunicorn {{ project }}.wsgi""")
