af58aba8ac8cadf52cea3bfbea09e040115d9b49
Leo Neto <lneto@sharpencx.com>
2020-07-21 21:17:11 -0400

    Init decorators

--- cli/decorators/__init__.py
+++ cli/decorators/__init__.py
@@ -2,6 +2,6 @@ from .cache import cache
 from .count_calls import count_calls
 from .lazy import lazy
 from .register import register
-from .timed import timed
-from .template import watch_templates
 from .singleton import singleton
+from .template import watch_templates
+from .timed import timed
