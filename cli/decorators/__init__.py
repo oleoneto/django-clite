ad622657b8a36456835cfc2407af5228689a8700
Leo Neto <oleoneto@gmail.com>
2023-05-01 18:08:34 -0300

    DCLI - refactor: remove decorators

--- cli/decorators/__init__.py
+++ cli/decorators/__init__.py
@@ -1,7 +1,3 @@
-from .cache import cache
-from .count_calls import count_calls
-from .lazy import lazy
-from .register import register
+# cli:decorators
 from .singleton import singleton
 from .template import watch_templates
-from .timed import timed
