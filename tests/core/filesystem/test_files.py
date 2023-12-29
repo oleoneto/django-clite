6a697557cde55c08f9cee7936075e66abaededf7
Leo Neto <oleoneto@gmail.com>
2023-12-29 00:24:50 -0300

    test: file creation

---
+++ tests/core/filesystem/test_files.py
@@ -0,0 +1,23 @@
+import unittest
+from click.testing import CliRunner
+from cli.core.filesystem.files import File
+
+runner = CliRunner()
+
+
+class FileTestCase(unittest.TestCase):
+    def test_create_file(self):
+        with runner.isolated_filesystem():
+            file = File("new.scratch", content="{{greeting}} world!", context={"greeting": "Hello"})
+            file.create()
+
+            self.assertTrue(file.path().exists())
+            self.assertEqual("Hello world!", file.contents())
+
+    def test_create_file_and_intermittent_directories(self):
+        with runner.isolated_filesystem():
+            file = File("files/1/2/new.scratch", content="{{greeting}} world!", context={"greeting": "Hello"})
+            file.create()
+
+            self.assertTrue(file.path().exists())
+            self.assertEqual("Hello world!", file.contents())
