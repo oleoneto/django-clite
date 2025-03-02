825ec0283f8bec4a50778ea76a713486a247a4ca
Leo Neto <oleoneto@gmail.com>
2025-03-02 01:01:40 -0300

    chore: remove directory creation test

--- tests/commands/new/test_new.py
+++ tests/commands/new/test_new.py
@@ -11,54 +11,6 @@ runner = CliRunner()
 
 
 class CreatorTestCase(unittest.TestCase):
-    def test_new_directory(self):
-        with runner.isolated_filesystem():
-            name = "store"
-            context = {"project": "store", "author": "Leo Neto"}
-
-            inner_proj = Directory(name)
-
-            proj = Directory(
-                name=name,
-                children=[
-                    inner_proj,
-                    Directory(name="staticfiles"),
-                    Directory(
-                        name="templates",
-                        children=[
-                            File(name="404.html", template="# {{ project }}"),
-                            File(name="500.html", template="# {{ project }}"),
-                        ],
-                    ),
-                    File(name="README.md", template="{{project}}/readme.tpl"),
-                    File(name="README.txt", template="project/readme.tpl"),
-                    File(name="a.py", template="# {{ project }}"),
-                    File(name="b.py", content="# content"),
-                ],
-            )
-
-            proj.create(**context)
-
-            self.assertTrue(proj.path().exists())
-            self.assertTrue(proj.path().is_dir())
-            self.assertEqual(3, len(proj.dirs))
-            self.assertEqual(4, len(proj.files))
-
-            # Template files:
-            self.assertEqual(
-                "store/readme.tpl\n", proj.files[0].path(proj.path()).read_text()
-            )
-
-            with open(Path(name) / "README.txt") as f:
-                line = f.readline()
-                self.assertEqual("# store\n", line)
-
-            # Template literal
-            self.assertEqual("# store\n", proj.files[2].path(proj.path()).read_text())
-
-            # Content literal
-            self.assertEqual("# content\n", proj.files[3].path(proj.path()).read_text())
-
     def test_new_project(self):
         with runner.isolated_filesystem():
             command = "project"
